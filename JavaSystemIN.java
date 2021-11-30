4331a0a66657fefb8a1d1e472be7f4ef8d3018fd
Dr232 <76934208+Dr232@users.noreply.github.com>
2021-11-30 19:56:56 +0800

    add a file

---
+++ JavaSystemIN.java
@@ -0,0 +1,19 @@
+import java.io.InputStream;
+
+public class JavaSystemIN {
+    //    System.in系统输入
+    public static void main(String[] args) throws Exception {
+//        InputStream input = System.in;								// 键盘输入流
+//        System.out.print("请输入信息：");								// 提示信息
+//        byte[] data = new byte[1024];									// 数据缓冲区
+//        int len = input.read(data);									// 读取数据
+//        System.out.println("输入内容为：" + new String(data, 0, len));	// 打印输入内容
+//    }
+//}
+//        try {
+//            Integer.parseInt("abc");
+//        } catch (Exception e) {
+            System.err.println("hahah");
+        }
+    }
+//}
